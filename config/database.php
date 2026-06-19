22d13bc9c8286ca653131ce006cb8a34deadd5f7
abdu1198 <ewwe1198@gmail.com>
2026-06-19 16:50:49 +0300

    Configure mysql pdo connection options with SSL CA and Emulate Prepares

--- config/database.php
+++ config/database.php
@@ -59,7 +59,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                \PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                \PDO::ATTR_EMULATE_PREPARES => true, // يحل مشاكل التوافق مع الأنواع الحديثة
+            ]) : [],
         ],
         'mariadb' => [
             'driver' => 'mariadb',
